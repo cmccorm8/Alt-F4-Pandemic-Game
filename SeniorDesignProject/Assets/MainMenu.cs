9e830f5232de68cface973081c6c5a4b2148aa17
Siddharth Meel <60525291+SiddharthMeel@users.noreply.github.com>
2021-03-09 10:25:20 -0800

    Added Main Menu Scene and Pause Menu
    Implementation of a start screen and a pause screen was done.

---
+++ SeniorDesignProject/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour
+{
+    public string startLevel;
+
+    public string levelSelect;
+
+    public void NewGame()
+    {
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
