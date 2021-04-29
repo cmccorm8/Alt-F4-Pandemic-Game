730ba71264a03a43f3be8c711fe273929ef7a769
gbrown15 <gbrown15@uwyo.edu>
2021-04-28 20:42:41 -0600

    Pause menu completed and implemented in SuperMarket
    Also added a nifty little drink cooler to SuperMarket

--- SeniorDesignProject/Assets/PauseMenu.cs
+++ SeniorDesignProject/Assets/PauseMenu.cs
@@ -9,43 +9,45 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
-    public GameObject pauseMenuUI ;
+    public GameObject pauseMenuUI;
 
-    void Update() {
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
                 Resume();
-            } else
+            }
+            else
             {
                 Pause();
             }
         }
     }
 
-  void Resume ()
-  {
-      pauseMenuUI.SetActive(false);
-      Time.timeScale = 1f;
-      GameIsPaused = false;
-  }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
-void Pause ()
-  {
-      pauseMenuUI.SetActive(true);
-      Time.timeScale = 0f;
-      GameIsPaused = true;
-  }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
 
-public void LoadMenu()
-{
-    Time.timeScale = 1f;
-    SceneManager.LoadScene("MainMenu");
-}
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("title_menu");
+    }
 
-public void QuitGame()
-{
-    Application.Quit();
-}
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
