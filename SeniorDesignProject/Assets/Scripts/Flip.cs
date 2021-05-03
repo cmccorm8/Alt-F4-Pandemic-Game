f2bfccc2e8dcb4bdb6ce7e728192723db9e12959
Chance McCormick <chancemccormick@outlook.com>
2021-05-02 19:37:38 -0600

    File Organization

---
+++ SeniorDesignProject/Assets/Scripts/Flip.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    //public GameObject enemy;
+    public Transform enemyTransform;
+
+    public Transform player;
+    public Rigidbody2D enemyRigidbody;
+
+    private bool enemyFlipped = false;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = enemyRigidbody.velocity;
+        Vector3 enemyScale = enemyTransform.localScale;
+        
+        //Rotates enemy based on player position
+        if(enemyTransform.position.x < player.position.x && enemyFlipped)
+        {
+            enemyTransform.localScale = enemyScale;
+            enemyTransform.Rotate(0f, 180f, 0f);
+            enemyFlipped = false;
+        }
+        else if(enemyTransform.position.x > player.position.x && !enemyFlipped)
+        {
+            enemyTransform.localScale = enemyScale;
+            enemyTransform.Rotate(0f, 180f, 0f);
+            enemyFlipped = true;
+        }
+
+
+
+    }
+}
